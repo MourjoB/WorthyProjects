c881dc32e73a1033a8f26a5cd584c2111b4f000a
MourjoB <142159668+MourjoB@users.noreply.github.com>
2023-08-31 16:37:05 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<string>
+#include<cstring>
+#include<sstream>
+using namespace std;
+
+
+
+int main(){
+int a;
+cin>>a;
+stringstream helo;
+helo<<a;
+string b;
+helo>>b;
+cout<<b+"water";
+
+
+    return 0;
+}
